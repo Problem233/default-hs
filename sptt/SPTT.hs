092ee15cf16063c1ea78112dfa5f2e4fcfbe9209
problem233 <problem233@outlook.com>
2017-03-05 16:08:41 +0800

    fix

--- sptt/SPTT.hs
+++ sptt/SPTT.hs
@@ -5,13 +5,15 @@ import Math
 main :: IO ()
 main = do
   putStrLn "λ> 欢迎使用 `searchPythagoreanTriple` 测试程序"
-  putStr "λ> 请输入要搜索的范围，用空格分隔: "
-  hFlush stdout
+  putStr' "λ> 请输入要搜索的范围，用空格分隔: "
   str <- getLine
+  putStr' "\n\n"
   let (fromStr, toStr) = span (/= ' ') str
-      [from, to] = map (read :: String -> Integer) [fromStr, tail toStr]
+      [from, to] = map (read :: String -> Integer)
+                       [fromStr, tail toStr]
    in forM_ [from..to] $ \x -> do
         putStrLn (show x ++ ":")
         forM_ (searchPythagoreanTriple x) $ \triple ->
           putStrLn $ "  " ++ show triple
         putStrLn ""
+  where putStr' str = putStr str >> hFlush stdout

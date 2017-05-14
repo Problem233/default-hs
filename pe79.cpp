0a8c0f0e5638dd4d1db4a3ca464fb71665ee9f20
problem233 <problem233@outlook.com>
2017-05-14 10:46:46 +0800

    improve

--- pe79.cpp
+++ pe79.cpp
@@ -6,21 +6,21 @@ using namespace std;
 
 int main() {
   string kl[50];
-  int apps[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  char apps[] = "0123456789";
   for(int i = 0; i < 50; i++) {
     cin >> kl[i];
-    apps[kl[i][0] - 48] = apps[kl[i][1] - 48] = apps[kl[i][2] - 48] = 10;
+    apps[kl[i][0] - 48] = apps[kl[i][1] - 48] = apps[kl[i][2] - 48] = 'x';
   }
-  vector<int> r;
+  vector<char> r;
   for(int j = 0; j < 10; j++)
-    if(apps[j] == 10) r.push_back(j);
-  sort(r.begin(), r.end(), [&kl] (const int &a, const int &b) {
+    if(apps[j] == 'x') r.push_back(j + 48);
+  sort(r.begin(), r.end(), [&kl] (const char &a, const char &b) {
     for(int i = 0; i < 50; i++)
-      if(b == kl[i][0] - 48 && (a == kl[i][1] - 48 || a == kl[i][2] - 48) ||
-         b == kl[i][1] - 48 && a == kl[i][2] - 48) return false;
+      if(b == kl[i][0] && (a == kl[i][1] || a == kl[i][2]) ||
+         b == kl[i][1] && a == kl[i][2]) return false;
     return true;
   });
-  for(vector<int>::iterator it = r.begin(); it != r.end(); it++)
+  for(vector<char>::iterator it = r.begin(); it != r.end(); it++)
     cout << *it;
   cout << endl;
 }

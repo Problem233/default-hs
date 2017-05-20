a9e383b090ad3da8986b6e4d9c005ec03c332432
problem233 <problem233@outlook.com>
2017-05-20 11:40:39 +0800

    optimize

--- cw_is_my_friend_cheating.cpp
+++ cw_is_my_friend_cheating.cpp
@@ -2,16 +2,19 @@
 
 using namespace std;
 
+long long ceildiv(long long a, long long b) {
+  return (a - 1) / b + 1;
+}
+
 class RemovedNumbers {
   public:
 	  static vector<vector<long long>> removNb(long long n) {
       vector<vector<long long>> r;
       long long sum = (1 + n) * n / 2;
-      for(long long x = 1; x <= n; x++) {
-        if((sum - x) % (x + 1) == 0) {
-          long long y = (sum - x) / (x + 1);
-          if(y <= n) r.push_back({x, y});
-        }
+      long long min = ceildiv(n * (n - 1), (2 * (n + 1)));
+      for(long long x = min; x <= n; x++) {
+        if((sum - x) % (x + 1) == 0)
+          r.push_back({x, (sum - x) / (x + 1)});
       }
       return r;
     }

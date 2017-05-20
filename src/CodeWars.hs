22f765593f3fc2e652951d85abb2cda12a707189
problem233 <problem233@outlook.com>
2017-05-20 09:55:06 +0800

    add is-my-friend-cheating

--- src/CodeWars.hs
+++ src/CodeWars.hs
@@ -8,3 +8,11 @@ solequa n = [(a, b) | x <- [1..truncate $ sqrt $ fromIntegral n],
                       (ndx - x) `mod` 4 == 0,
                       let b = (ndx - x) `div` 4
                           a = x + 2 * b]
+
+-- https://www.codewars.com/kata/is-my-friend-cheating/
+-- It would fail because of the ordering problem.
+removNb :: Integer-> [(Integer, Integer)]
+removNb n = let s = sum [1..n]
+             in foldl (\r (x, y) -> if x * y == s - x - y
+                                    then (x, y) : (y, x) : r else r)
+                      [] [(x, y) | x <- [1..n], y <- [x + 1..n]]

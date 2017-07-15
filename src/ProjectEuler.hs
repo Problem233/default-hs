ab0471aa696863ceec7364c65ec3471ed4d0bf12
problem233 <problem233@outlook.com>
2017-07-15 11:20:49 +0800

    edit

--- src/ProjectEuler.hs
+++ src/ProjectEuler.hs
@@ -102,8 +102,7 @@ answer3_1 = find num 2 2
 answer4_1 :: Integer
 answer4_1 = foldl max 0 $
             filter (\xs -> reverse (show xs) == show xs)
-                   [x * y | x <- reverse [100..999],
-                            y <- reverse [100..x]]
+                   [x * y | x <- reverse [100..999], y <- reverse [100..x]]
 
 -- Problem 5: Smallest multiple
 -- --
@@ -123,9 +122,9 @@ answer5_1 = product $
 -- Problem 6: Sum square difference
 -- --
 -- The sum of the squares of the first ten natural numbers is,
--- 12 + 22 + ... + 102 = 385
+-- 1^2 + 2^2 + ... + 10^2 = 385
 -- The square of the sum of the first ten natural numbers is,
--- (1 + 2 + ... + 10)2 = 552 = 3025
+-- (1 + 2 + ... + 10)^2 = 55^2 = 3025
 -- Hence the difference between the sum of the squares of the first ten natural
 -- numbers and the square of the sum is 3025 − 385 = 2640.
 -- Find the difference between the sum of the squares of the first one hundred
@@ -142,7 +141,7 @@ answer6_1 = sqr (sum [1..100]) - sum [sqr x | x <- [1..100]]
 -- --
 -- By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see
 -- that the 6th prime is 13.
--- What is the 10 001st prime number?
+-- What is the 10001st prime number?
 -- --
 -- Answer: 104743
 

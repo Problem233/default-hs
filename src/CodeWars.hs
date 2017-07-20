d2d6230da60cbca4274d4cdee36d63a1dba821b1
problem233 <problem233@outlook.com>
2017-07-20 16:28:48 +0800

    use do-notation

--- src/CodeWars.hs
+++ src/CodeWars.hs
@@ -1,21 +1,26 @@
 module CodeWars where
 
+import Control.Monad (guard) -- Widely used
 import Data.Char (digitToInt) -- Used by revRot
 
 -- https://www.codewars.com/kata/diophantine-equation/
 solequa :: Integer -> [(Integer, Integer)]
-solequa n = [(a, b) | x <- [1..truncate $ sqrt $ fromIntegral n],
-                      n `mod` x == 0,
-                      let ndx = n `div` x,
-                      (ndx - x) `mod` 4 == 0,
-                      let b = (ndx - x) `div` 4
-                          a = x + 2 * b]
+solequa n = do
+  x <- [1..truncate $ sqrt $ fromIntegral n]
+  guard $ n `mod` x == 0
+  let ndx = n `div` x
+      (b, r) = (ndx - x) `quotRem` 4
+  guard $ r == 0
+  let a = x + 2 * b
+  return (a, b)
 
 -- https://www.codewars.com/kata/is-my-friend-cheating/
 removNb :: Integer -> [(Integer, Integer)]
-removNb n = [(x, y) | x <- [min..n],
-                      (sum - x) `mod` (x + 1) == 0,
-                      let y = (sum - x) `div` (x + 1)]
+removNb n = do
+  x <- [min..n]
+  let (y, r) = (sum - x) `quotRem` (x + 1)
+  guard $ r == 0
+  return (x, y)
   where sum = (1 + n) * n `div` 2
         min = let fn = fromIntegral n
                in ceiling $ fn * (fn - 1) / (2 * (fn + 1))

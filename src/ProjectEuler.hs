65dc69c64c285446e69000c09beb4aadd05a93a0
problem233 <problem233@outlook.com>
2017-08-24 18:23:33 +0800

    edit

--- src/ProjectEuler.hs
+++ src/ProjectEuler.hs
@@ -726,7 +726,7 @@ answer97_1 = (28433 * 2 ^ 7830457 + 1) `mod` 10000000000
 -- polynomial functions that can model the sequence.
 -- As an example, let us consider the sequence of cube numbers. This is defined
 -- by the generating function,
--- un = n3: 1, 8, 27, 64, 125, 216, ...
+-- u_n = n ^ 3: 1, 8, 27, 64, 125, 216, ...
 -- Suppose we were only given the first two terms of this sequence. Working on
 -- the principle that "simple is best" we should assume a linear relationship
 -- and predict the next term to be 15 (common difference 7). Even if we were
@@ -735,10 +735,10 @@ answer97_1 = (28433 * 2 ^ 7830457 + 1) `mod` 10000000000
 -- We shall define OP(k, n) to be the nth term of the optimum polynomial
 -- generating function for the first k terms of a sequence. It should be clear
 -- that OP(k, n) will accurately generate the terms of the sequence for n ≤ k,
--- and potentially the first incorrect term (FIT) will be OP(k, k+1); in which
+-- and potentially the first incorrect term (FIT) will be OP(k, k + 1); in which
 -- case we shall call it a bad OP (BOP).
 -- As a basis, if we were only given the first term of sequence, it would be
--- most sensible to assume constancy; that is, for n ≥ 2, OP(1, n) = u1.
+-- most sensible to assume constancy; that is, for n ≥ 2, OP(1, n) = u_1.
 -- Hence we obtain the following OPs for the cubic sequence:
 -- OP(1, n) = 1 	                  1, 1, 1, 1, ...
 -- OP(2, n) = 7 n − 6 	            1, 8, 15, ...

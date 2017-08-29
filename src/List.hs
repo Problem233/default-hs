2557f36fceb33bf57a893ee7be1d50aa2ac23b9e
problem233 <problem233@outlook.com>
2017-08-29 19:35:10 +0800

    add splitOn splitOnString

--- src/List.hs
+++ src/List.hs
@@ -1,6 +1,19 @@
-module List (split) where
+module List (split, splitOn, splitOnString) where
 
 split :: (a -> Bool) -> [a] -> [[a]]
-split p s = case break p s of
+split p xs = case break p xs of
               (l, _ : r) -> l : split p r
               (l, []) -> [l]
+
+splitOn :: Eq a => a -> [a] -> [[a]]
+splitOn x = split (== x)
+
+splitOnString :: Eq a => [a] -> [a] -> [[a]]
+splitOnString _ [] = []
+splitOnString x xs = recursion (length x) xs
+  where recursion _ [] = [[]]
+        recursion len xs @ (h : xs')
+          | l == x = [] : recursion len r
+          | otherwise = let (h' : xs'') = recursion len xs'
+                         in (h : h') : xs''
+          where (l, r) = splitAt len xs

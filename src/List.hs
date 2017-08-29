44223779a8f5565298add1affa37d5a79fc1ed49
problem233 <problem233@outlook.com>
2017-08-29 19:18:17 +0800

    fix

--- src/List.hs
+++ src/List.hs
@@ -1,6 +1,6 @@
 module List (split) where
 
 split :: (a -> Bool) -> [a] -> [[a]]
-split _ [] = []
-split p s = let (l, _ : r) = break p s
-             in l : split p r
+split p s = case break p s of
+              (l, _ : r) -> l : split p r
+              (l, []) -> [l]

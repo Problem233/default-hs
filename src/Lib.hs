7505440ce440070a623427ce37f3a61005674f00
problem233 <problem233@outlook.com>
2018-10-21 15:33:02 +0800

    add spin

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,8 @@
 module Lib (
   module Csound.Base,
   renderSnd,
-  wind) where
+  wind,
+  spin, spinFull) where
 
 import Csound.Base
 
@@ -17,3 +18,19 @@ renderSnd = writeSndBy $ setRates 48000 64 <>
 
 wind :: SE Sig
 wind = osc . (\x -> 220 + 88 * (osc x + x)) <$> pink
+
+-- | Spins the sound.
+spin :: Sig -- ^ The amount of the sound saved in both left and right. 
+            -- This argument should be @< 1@ and @>= 0@,
+            -- or unknown error may occur.
+     -> Sig -- ^ The frequency of spinning.
+     -> Sig -- ^ The input signal.
+     -> (Sig, Sig)
+spin save freq sig =
+  let save' = 2 * save / (save - 2)
+      left = (osc freq + 1 + save') / (2 + save')
+      right = 1 - left
+   in (left * sig, right * sig)
+
+spinFull :: Sig -> Sig -> (Sig, Sig)
+spinFull = spin 0
